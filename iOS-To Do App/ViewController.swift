2c23b318634f18ebef73f10f47046772181ae9f2
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-08-15 13:51:47 -0500

    Discarded all changes to create actual To-Do App, put UI items in place on storyboard

--- iOS-To Do App/ViewController.swift
+++ iOS-To Do App/ViewController.swift
@@ -9,35 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-	@IBOutlet weak var myTextField: UITextField!
-	@IBOutlet weak var myLabel: UILabel!
-	// the ! means I know otherLabel can possibly be nil, but it's not going to be nil
-	@IBOutlet var otherLabel: UILabel!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Assign ViewController as a delegate of the text field in StoryBoard, current, or with:
-		// myTextField.delegate = self
+		
 	}
 	
-	@IBAction func didTapOtherButton(_ sender: Any) {
-		let name = myTextField.text!
-		otherLabel.text = "Hi \(name)!"
-		myTextField.resignFirstResponder()
-	}
-	
-	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-		view.endEditing(true)
-	}
-	
-	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-		myTextField.resignFirstResponder()
-		return false
-	}
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
 	}
 
 
